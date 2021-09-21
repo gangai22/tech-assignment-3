3244b2c073c89d929943e0a3a3df9603daacd03a
gangai22 <gangaidzotich123@gmail.com>
2021-09-20 21:23:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Author:Tichaona Gangaidzo 
+// Date: 9/20/2021
+// Tech Assignment #3: Use of iterative statements while getting input from user
+
+using System;
+
+namespace tech_assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Helllo, please enter a kilometer value ranging between 10 and 25");
+
+        }
+    }
+}
